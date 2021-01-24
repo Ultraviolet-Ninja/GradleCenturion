fc485ec6ffb73774b4385bb8f99d037fad350ace
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-01-23 18:20:44 -0600

    Created some simple test for Alphabet

--- src/test/java/bomb/modules/ab/AlphabetTest.java
+++ src/test/java/bomb/modules/ab/AlphabetTest.java
@@ -1,4 +1,18 @@
 package bomb.modules.ab;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AlphabetTest {
+    private static final String[] FOUR_LETTER_INPUTS = {"XZQJ", "KOVB", "QJPS", "RINM", "YDXQ", "FGAR"},
+            FOUR_LETTER_CORRECT = {"JQXZ", "OKBV", "PQJS", "IRNM", "QYDX", "ARGF"};
+
+    @Test
+    void fourLetterTest(){
+        IntStream.range(0, FOUR_LETTER_CORRECT.length)
+                .forEach(i -> assertEquals(FOUR_LETTER_CORRECT[i], Alphabet.order(FOUR_LETTER_INPUTS[i])));
+    }
 }
