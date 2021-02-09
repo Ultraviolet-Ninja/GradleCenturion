33c9f528be5438ac4f0820a11e476621aaf2a88e
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-02-09 12:39:41 -0600

    Added all video tests for ChordQualitiesTest

--- src/test/java/bomb/modules/c/ChordQualitiesTest.java
+++ src/test/java/bomb/modules/c/ChordQualitiesTest.java
@@ -15,6 +15,11 @@ public class ChordQualitiesTest {
     @Test
     void videoTest(){
         assertEqualsAnyOrder("New Chord: A +2 +2 +3", new String[]{"D#", "F", "G#", "A"});
+        assertEqualsAnyOrder("New Chord: F +3 +1 +6", new String[]{"A#", "D", "D#", "F#"});
+        assertEqualsAnyOrder("New Chord: D# +3 +5 +3", new String[]{"A", "B", "C#", "E"});
+        assertEqualsAnyOrder("New Chord: E +2 +1 +4", new String[]{"G#", "C#", "D#", "E"});
+        assertEqualsAnyOrder("New Chord: E +4 +3 +4", new String[]{"A", "D", "E", "F"});
+        assertEqualsAnyOrder("New Chord: D +2 +1 +4", new String[]{"B", "C#", "F#", "G#"});
     }
 
     private void assertEqualsAnyOrder(String expected, String[] notes){
