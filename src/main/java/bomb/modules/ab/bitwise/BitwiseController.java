43433898254998fdc6a8c226f18a2316332718ff
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-04-04 20:31:52 -0500

    Optimized the BitwiseController code Took out method references in bitwise_ops.fxml

--- src/main/java/bomb/modules/ab/bitwise/BitwiseController.java
+++ src/main/java/bomb/modules/ab/bitwise/BitwiseController.java
@@ -1,10 +1,13 @@
 package bomb.modules.ab.bitwise;
 
 import bomb.tools.FacadeFX;
+import bomb.tools.HoverHandler;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import java.util.function.Consumer;
 
 public class BitwiseController {
     @FXML
@@ -13,24 +16,23 @@ public class BitwiseController {
     @FXML
     private TextField bitOut;
 
-    //FIXME
-    @FXML
-    private void bitwiseSolve(){
-        BitwiseOps bit = null;
-        if (bitAnd.isHover()){
-            bit = BitwiseOps.AND;
-        } else if (bitOr.isHover()){
-            bit = BitwiseOps.OR;
-        } else if (bitXor.isHover()){
-            bit = BitwiseOps.XOR;
-        } else if (bitNot.isHover()){
-            bit = BitwiseOps.NOT;
-        }
-        try {
-            bitOut.setText(Bitwise.getByte(bit));
-        } catch (IllegalArgumentException illegal){
-            FacadeFX.setAlert(Alert.AlertType.INFORMATION, illegal.getMessage(),
-                    "Something's empty", "Do more edge work");
-        }
+    public void initialize(){
+        HoverHandler<ActionEvent> handler = new HoverHandler<>(initAction());
+        bitAnd.setOnAction(handler);
+        bitNot.setOnAction(handler);
+        bitOr.setOnAction(handler);
+        bitXor.setOnAction(handler);
+    }
+
+    private Consumer<ActionEvent> initAction(){
+        return event -> {
+            BitwiseOps source = BitwiseOps.valueOf(((Button) event.getSource()).getText().toUpperCase());
+            try {
+                bitOut.setText(Bitwise.getByte(source));
+            } catch (IllegalArgumentException illegal){
+                FacadeFX.setAlert(Alert.AlertType.INFORMATION, illegal.getMessage(),
+                        "Something's empty", "Do more edge work");
+            }
+        };
     }
 }
