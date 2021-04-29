895ce65e4509b616b3dc081dbb5405e70276944a
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-04-28 23:29:43 -0500

    Started on the FastController and GUI

--- src/main/java/bomb/modules/dh/fast_math/FastController.java
+++ src/main/java/bomb/modules/dh/fast_math/FastController.java
@@ -1,4 +1,52 @@
 package bomb.modules.dh.fast_math;
 
+import bomb.tools.FacadeFX;
+import bomb.tools.HoverHandler;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.util.function.Consumer;
+
 public class FastController {
+    private final StringBuilder appendLetters = new StringBuilder(),
+            textFieldTracker = new StringBuilder();
+
+    @FXML
+    private Button alfa, bravo, charlie, delta, echo, golf, kilo, november, papa, sierra, tango, xRay, zulu;
+
+    @FXML
+    private TextField outputMath;
+
+    public void initialize(){
+//        FacadeFX.bindHandlerToButtons(new HoverHandler<>(initializeAction()), alfa, bravo, charlie, delta, echo,
+//                golf, kilo, november, papa, sierra, tango, xRay, zulu);
+    }
+
+    private Consumer<ActionEvent> initializeAction(){
+        return event -> {
+            String temp = ((Button) event.getSource()).getText();
+            appendLetters.append(temp);
+            textFieldTracker.append(temp);
+            try {
+                if (appendLetters.length() == 2) {
+                    textFieldTracker.append(" - ").append(FastMath.solve(appendLetters.toString()));
+                    appendLetters.setLength(0);
+                }
+                outputMath.setText(textFieldTracker.toString());
+                if (textFieldTracker.length() > 2) textFieldTracker.setLength(0);
+            } catch (IllegalArgumentException illegal){
+                FacadeFX.setAlert(Alert.AlertType.ERROR, illegal.getLocalizedMessage());
+            }
+        };
+    }
+
+    @FXML
+    private void clear(){
+        FacadeFX.clearText(outputMath);
+        textFieldTracker.setLength(0);
+        appendLetters.setLength(0);
+    }
 }
