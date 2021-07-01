b8348196322d7502321fe601882e9c451410bab2
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-07-01 16:54:58 -0500

    Fixed the Regex that deals with Serial Code validation Implemented the Iterable interface in the ReadOnlyRing Deleted one method in HexGrid

--- src/main/java/bomb/tools/data/structures/ring/ReadOnlyRing.java
+++ src/main/java/bomb/tools/data/structures/ring/ReadOnlyRing.java
@@ -2,29 +2,33 @@ package bomb.tools.data.structures.ring;
 
 import java.nio.BufferOverflowException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class ReadOnlyRing<E> {
-    private static class Node<E>{
+public class ReadOnlyRing<E> implements Iterable<E> {
+    private static class Node<E> {
         private final E data;
         private Node<E> prev, next;
 
-        private Node(E data){
+        private Node(E data) {
             this.data = data;
             prev = next = null;
         }
     }
 
     private final int capacity;
+    private boolean isCompletedRing;
     private int size;
     private Node<E> head;
 
-    public ReadOnlyRing(int capacity){
+    public ReadOnlyRing(int capacity) {
         this.capacity = capacity;
         size = 0;
         head = null;
+        isCompletedRing = false;
     }
 
-    public boolean add(E element) throws BufferOverflowException{
+    public boolean add(E element) throws BufferOverflowException {
         if (size <= capacity) {
             if (size == 0)
                 head = new Node<>(element);
@@ -38,25 +42,26 @@ public class ReadOnlyRing<E> {
         return size != capacity;
     }
 
-    private void addNext(E element){
+    private void addNext(E element) {
         Node<E> temp = head;
         while (temp.next != null) temp = temp.next;
         temp.next = new Node<>(element);
         temp.next.prev = temp;
     }
 
-    private void addLast(E element){
+    private void addLast(E element) {
         Node<E> temp = head;
         while (temp.next != null) temp = temp.next;
         temp.next = new Node<>(element);
         temp.next.prev = temp;
         temp.next.next = head;
         head.prev = temp.next;
+        isCompletedRing = true;
     }
 
-    public int findIndex(E element){
+    public int findIndex(E element) {
         Node<E> temp = head;
-        for (int i = 0; i < capacity; i++){
+        for (int i = 0; i < capacity; i++) {
             if (element.equals(temp.data))
                 return i;
             temp = temp.next;
@@ -64,19 +69,19 @@ public class ReadOnlyRing<E> {
         return -1;
     }
 
-    public E getHeadData(){
+    public E getHeadData() {
         return head.data;
     }
 
-    public void rotateHeadClockwise(){
+    public void rotateHeadClockwise() {
         head = head.next;
     }
 
-    public void rotateHeadCounter(){
+    public void rotateHeadCounter() {
         head = head.prev;
     }
 
-    public ArrayList<E> toArrayList(){
+    public List<E> toList() {
         ArrayList<E> outputList = new ArrayList<>();
         Node<E> temp = head;
         while (temp.next != head) {
@@ -84,5 +89,10 @@ public class ReadOnlyRing<E> {
             temp = temp.next;
         }
         return outputList;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return toList().iterator();
     }
 }
