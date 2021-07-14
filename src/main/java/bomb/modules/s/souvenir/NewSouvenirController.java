bf205ef829026d52ced5c7e5e203250570945ed3
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-07-13 22:19:11 -0500

    Setup the new_souvenir.fxml correctly Almost done with the NewSouvenirController Started the souvenir.css

--- src/main/java/bomb/modules/s/souvenir/NewSouvenirController.java
+++ src/main/java/bomb/modules/s/souvenir/NewSouvenirController.java
@@ -2,28 +2,42 @@ package bomb.modules.s.souvenir;
 
 import bomb.abstractions.Resettable;
 import com.jfoenix.controls.JFXTextArea;
-import com.jfoenix.controls.JFXTreeTableView;
 import javafx.fxml.FXML;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Pair;
+import org.controlsfx.control.tableview2.TableColumn2;
+import org.controlsfx.control.tableview2.TableView2;
 
 import java.util.List;
 
 public class NewSouvenirController implements Resettable {
-    @FXML private JFXTreeTableView<?> artifactView;
+    @FXML private TableView2<Pair<String, String>> artifactView;
 
-    @FXML private TreeTableColumn<?, ?> key, answer;
+    @FXML private TableColumn2<Pair<String, String>, String> keyColumn, answerColumn;
 
     @FXML private JFXTextArea searchArea;
+
+    public void initialize() {
+        keyColumn.setCellValueFactory(new PropertyValueFactory<>("key"));
+        answerColumn.setCellValueFactory(new PropertyValueFactory<>("value"));
+        searchArea.setTextFormatter(createNewLineRestrictionFormatter());
+    }
+
+    private TextFormatter<String> createNewLineRestrictionFormatter(){
+        return new TextFormatter<>(change -> {
+            if (!change.isContentChange()) return change;
+            if (change.getControlNewText().contains("\n")) return null;
+
+            return change;
+        });
+    }
 
     public void liveUpdate() {
         List<Pair<String, String>> injectionList = NewSouvenir.updateTableView();
         if (!injectionList.isEmpty()){
             for (Pair<String, String> pair : injectionList){
-                TreeItem<String> keyItem = new TreeItem<>(pair.getKey());
-                TreeItem<String> answerItem = new TreeItem<>(pair.getValue());
-
+                artifactView.getItems().add(pair);
             }
         }
     }
@@ -36,5 +50,6 @@ public class NewSouvenirController implements Resettable {
     @Override
     public void reset() {
         NewSouvenir.reset();
+        //TODO - Reset the columns
     }
 }
