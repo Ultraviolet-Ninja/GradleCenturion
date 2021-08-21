811474a247c6c2be2b85d0c3f9fde8bf11b05f69
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-08-21 17:23:11 -0500

    Refurbished Emoji Math

--- src/main/java/bomb/modules/dh/emoji/EmojiControllerState.java
+++ src/main/java/bomb/modules/dh/emoji/EmojiControllerState.java
@@ -3,17 +3,46 @@ package bomb.modules.dh.emoji;
 import bomb.abstractions.State;
 
 public enum EmojiControllerState implements State<EmojiControllerState> {
-    START {
+    FIRST_EMOJI_PRESS {
+        @Override
+        public EmojiControllerState nextState() {
+            return SECOND_EMOJI_PRESS;
+        }
+    },
+    SECOND_EMOJI_PRESS {
+        @Override
+        public EmojiControllerState nextState() {
+            return MATH_SYMBOL_PRESS;
+        }
+    },
+    MATH_SYMBOL_PRESS {
+        @Override
+        public EmojiControllerState nextState() {
+            return THIRD_EMOJI_PRESS;
+        }
+    },
+    THIRD_EMOJI_PRESS {
+        @Override
+        public EmojiControllerState nextState() {
+            return FOURTH_EMOJI_PRESS;
+        }
+    },
+    FOURTH_EMOJI_PRESS {
+        @Override
+        public EmojiControllerState nextState() {
+            return END;
+        }
+    },
+    END {
+        @Override
+        public EmojiControllerState nextState() {
+            return RESET;
+        }
+    },
+    RESET {
         @Override
         public EmojiControllerState nextState() {
             return FIRST_EMOJI_PRESS;
         }
-    },
-
-    FIRST_EMOJI_PRESS {
-        @Override
-        public EmojiControllerState nextState() {
-            return null;
-        }
-    };
+    }
 }
