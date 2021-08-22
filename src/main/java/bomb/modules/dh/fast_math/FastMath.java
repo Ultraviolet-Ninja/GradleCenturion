dd3f82e0f6e849b8ddb93963e20101d9326fac60
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-08-22 17:41:30 -0500

    Finished FastMath Updated the version number to reflect number of completed puzzles Updated Progress.md

--- src/main/java/bomb/modules/dh/fast_math/FastMath.java
+++ src/main/java/bomb/modules/dh/fast_math/FastMath.java
@@ -9,46 +9,61 @@ import static bomb.tools.filter.Filter.ultimateFilter;
 
 public class FastMath extends Widget {
     private static final int[][] INTERNAL_GRID = new int[][]{
-            {25,11,53,97,2,42,51,97,12,86,55,73,33}, {54,7,32,19,84,33,27,78,26,46,9,13,58},
-            {86,37,44,1,5,26,93,49,18,69,23,40,22}, {54,28,77,93,11,0,35,61,27,48,13,72,80},
-            {99,36,23,95,67,5,26,17,44,60,26,41,67}, {74,95,3,4,56,23,54,29,52,38,10,76,98},
-            {88,46,37,96,2,52,81,37,12,70,14,36,78}, {54,43,12,65,94,3,47,23,16,62,73,46,21},
-            {7,33,26,1,67,26,27,77,83,14,27,93,9}, {63,64,94,27,48,84,33,10,16,74,43,99,4},
-            {35,39,3,25,47,62,38,45,88,48,34,31,27}, {67,30,27,71,9,11,44,37,18,40,32,15,78},
-            {13,23,26,85,92,12,73,56,81,7,75,47,99}
+            {25, 11, 53, 97, 2, 42, 51, 97, 12, 86, 55, 73, 33}, {54, 7, 32, 19, 84, 33, 27, 78, 26, 46, 9, 13, 58},
+            {86, 37, 44, 1, 5, 26, 93, 49, 18, 69, 23, 40, 22}, {54, 28, 77, 93, 11, 0, 35, 61, 27, 48, 13, 72, 80},
+            {99, 36, 23, 95, 67, 5, 26, 17, 44, 60, 26, 41, 67}, {74, 95, 3, 4, 56, 23, 54, 29, 52, 38, 10, 76, 98},
+            {88, 46, 37, 96, 2, 52, 81, 37, 12, 70, 14, 36, 78}, {54, 43, 12, 65, 94, 3, 47, 23, 16, 62, 73, 46, 21},
+            {7, 33, 26, 1, 67, 26, 27, 77, 83, 14, 27, 93, 9}, {63, 64, 94, 27, 48, 84, 33, 10, 16, 74, 43, 99, 4},
+            {35, 39, 3, 25, 47, 62, 38, 45, 88, 48, 34, 31, 27}, {67, 30, 27, 71, 9, 11, 44, 37, 18, 40, 32, 15, 78},
+            {13, 23, 26, 85, 92, 12, 73, 56, 81, 7, 75, 47, 99}
     };
 
-    public static String solve(String letters){
+    public static String solve(String letters) {
         if (letters == null || letters.length() != 2)
             throw new IllegalArgumentException("Input 2 letters, please");
         serialCodeChecker();
         int preconditions = edgework();
-        int leftNum = translateLetter(letters.substring(0,1));
+        int leftNum = translateLetter(letters.substring(0, 1));
         int rightNum = translateLetter(letters.substring(1));
-        return String.valueOf(postConditions(INTERNAL_GRID[leftNum][rightNum] + preconditions));
+
+        String outputValue = String.valueOf(postConditions(INTERNAL_GRID[leftNum][rightNum] + preconditions));
+        return (outputValue.length() == 1 ? "0" : "") + outputValue;
     }
 
-    private static int translateLetter(String letter) throws IllegalArgumentException{
-        switch (letter.toUpperCase()){
-            case "A": return 0;
-            case "B": return 1;
-            case "C": return 2;
-            case "D": return 3;
-            case "E": return 4;
-            case "G": return 5;
-            case "K": return 6;
-            case "N": return 7;
-            case "P": return 8;
-            case "S": return 9;
-            case "T": return 10;
-            case "X": return 11;
-            case "Z": return 12;
+    private static int translateLetter(String letter) throws IllegalArgumentException {
+        switch (letter.toUpperCase()) {
+            case "A":
+                return 0;
+            case "B":
+                return 1;
+            case "C":
+                return 2;
+            case "D":
+                return 3;
+            case "E":
+                return 4;
+            case "G":
+                return 5;
+            case "K":
+                return 6;
+            case "N":
+                return 7;
+            case "P":
+                return 8;
+            case "S":
+                return 9;
+            case "T":
+                return 10;
+            case "X":
+                return 11;
+            case "Z":
+                return 12;
             default:
                 throw new IllegalArgumentException("Illegal letter given");
         }
     }
 
-    private static int edgework(){
+    private static int edgework() {
         int output = hasLitIndicator(Indicator.MSA) ? 20 : 0; //If the bomb has a lit MSA indicator
         output += portExists(Port.SERIAL) ? 14 : 0; //If the bomb has a Serial Port
         //If the serial number has the letters F A S T
@@ -58,7 +73,7 @@ public class FastMath extends Widget {
         return output;
     }
 
-    private static int postConditions(int num){
+    private static int postConditions(int num) {
         if (num > 100) return num % 100;
         if (num < 0) return num + 50;
         return num;
