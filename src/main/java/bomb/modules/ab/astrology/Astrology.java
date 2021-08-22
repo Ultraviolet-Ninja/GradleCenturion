5b50526e048c479af6ec3b4c3722f0087ac9db45
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-08-22 07:22:54 -0500

    Finished Astrology redesign

--- src/main/java/bomb/modules/ab/astrology/Astrology.java
+++ src/main/java/bomb/modules/ab/astrology/Astrology.java
@@ -20,33 +20,39 @@ public class Astrology extends Widget {
     public static final byte ELEMENT_INDEX = 0, CELESTIAL_INDEX = 1, ZODIAC_INDEX = 2, EXPECTED_SIZE = 3;
     public static final String GOOD_OMEN = "Good Omen at ", POOR_OMEN = "Poor Omen at ", NO_OMEN = "No Omen";
 
-    private static final int[][] STAGE_ONE = {{0,0,1,-1,0,1,-2,2,0,-1}, {-2,0,-1,0,2,0,-2,2,0,1},
-            {-1,-1,0,-1,1,2,0,2,1,-2}, {-1,2,-1,0,-2,-1,0,2,-2,2}},
+    private static final byte[][] ELEMENT_CELESTIAL_GRID = {
+            {0, 0, 1, -1, 0, 1, -2, 2, 0, -1}, {-2, 0, -1, 0, 2, 0, -2, 2, 0, 1},
+            {-1, -1, 0, -1, 1, 2, 0, 2, 1, -2}, {-1, 2, -1, 0, -2, -1, 0, 2, -2, 2}
+    },
 
-            STAGE_TWO = {{1,0,-1,0,0,2,2,0,1,0,1,0}, {2,2,-1,2,-1,-1,-2,1,2,0,0,2},
-                    {-2,-1,0,0,1,0,1,2,-1,-2,1,1}, {1,1,-2,-2,2,0,-1,1,0,0,-1,-1}},
+    ELEMENT_ZODIAC_GRID = {
+            {1, 0, -1, 0, 0, 2, 2, 0, 1, 0, 1, 0}, {2, 2, -1, 2, -1, -1, -2, 1, 2, 0, 0, 2},
+            {-2, -1, 0, 0, 1, 0, 1, 2, -1, -2, 1, 1}, {1, 1, -2, -2, 2, 0, -1, 1, 0, 0, -1, -1}
+    },
 
-            STAGE_THREE = {{-1,-1,2,0,-1,0,-1,1,0,0,-2,-2}, {-2,0,1,0,2,0,-1,1,2,0,1,0},
-                    {-2,-2,-1,-1,1,-1,0,-2,0,0,-1,1}, {-2,2,-2,0,0,1,-1,0,2,-2,-1,1},
-                    {-2,0,-1,-2,-2,-2,-1,1,1,1,0,-1}, {-1,-2,1,-1,0,0,0,1,0,-1,2,0},
-                    {-1,-1,0,0,1,1,0,0,0,0,-1,-1}, {-1,2,0,0,1,-2,1,0,2,-1,1,0},
-                    {1,0,2,1,-1,1,1,1,0,-2,2,0}, {-1,0,0,-1,-2,1,2,1,1,0,0,-1}};
+    CELESTIAL_ZODIAC_GRID = {
+            {-1, -1, 2, 0, -1, 0, -1, 1, 0, 0, -2, -2}, {-2, 0, 1, 0, 2, 0, -1, 1, 2, 0, 1, 0},
+            {-2, -2, -1, -1, 1, -1, 0, -2, 0, 0, -1, 1}, {-2, 2, -2, 0, 0, 1, -1, 0, 2, -2, -1, 1},
+            {-2, 0, -1, -2, -2, -2, -1, 1, 1, 1, 0, -1}, {-1, -2, 1, -1, 0, 0, 0, 1, 0, -1, 2, 0},
+            {-1, -1, 0, 0, 1, 1, 0, 0, 0, 0, -1, -1}, {-1, 2, 0, 0, 1, -2, 1, 0, 2, -1, 1, 0},
+            {1, 0, 2, 1, -1, 1, 1, 1, 0, -2, 2, 0}, {-1, 0, 0, -1, -2, 1, 2, 1, 1, 0, 0, -1}
+    };
 
     /**
      * Calculates the phrase composed of the Omen to press and when to press it
      *
      * @param symbols The input symbols
-     * @throws IllegalArgumentException Serial code is empty or there's an issue with the amount of symbols
      * @return The String command
+     * @throws IllegalArgumentException Serial code is empty or there's an issue with the amount of symbols
      */
-    public static String calculate(AstroSymbol... symbols)
-            throws IllegalArgumentException{
+    public static String calculate(AstroSymbol... symbols) throws IllegalArgumentException {
+        serialCodeChecker();
         checkThreeTypes(symbols);
         AstroSymbol[] sortSymbols = sort(symbols);
 
-        int first = STAGE_ONE[sortSymbols[ELEMENT_INDEX].getIdx()][sortSymbols[CELESTIAL_INDEX].getIdx()],
-                second = STAGE_TWO[sortSymbols[ELEMENT_INDEX].getIdx()][sortSymbols[ZODIAC_INDEX].getIdx()],
-                third = STAGE_THREE[sortSymbols[CELESTIAL_INDEX].getIdx()][sortSymbols[ZODIAC_INDEX].getIdx()];
+        int first = ELEMENT_CELESTIAL_GRID[sortSymbols[ELEMENT_INDEX].getIdx()][sortSymbols[CELESTIAL_INDEX].getIdx()],
+                second = ELEMENT_ZODIAC_GRID[sortSymbols[ELEMENT_INDEX].getIdx()][sortSymbols[ZODIAC_INDEX].getIdx()],
+                third = CELESTIAL_ZODIAC_GRID[sortSymbols[CELESTIAL_INDEX].getIdx()][sortSymbols[ZODIAC_INDEX].getIdx()];
 
         int results = first + second + third;
         results = checkMatchingSerialLetters(results, sortSymbols);
@@ -64,16 +70,14 @@ public class Astrology extends Widget {
      * the output value. If not, it'll subtract 1
      *
      * @param initialVal The output value
-     * @param symbols The set of AstroSymbols
+     * @param symbols    The set of AstroSymbols
      * @return The resulting output value
      * @throws IllegalArgumentException The Serial Code is empty
      */
     private static int checkMatchingSerialLetters(int initialVal, AstroSymbol[] symbols)
-            throws IllegalArgumentException{
-        if (serialCode.isEmpty()) throw new IllegalArgumentException("Serial Code is required");
-
+            throws IllegalArgumentException {
         String letters = ultimateFilter(serialCode, CHAR_FILTER);
-        for (AstroSymbol symbol : symbols){
+        for (AstroSymbol symbol : symbols) {
             Regex checker = new Regex("[" + letters + "]", symbol.name(), Pattern.CASE_INSENSITIVE);
             if (checker.findAllMatches().isEmpty()) initialVal--;
             else initialVal++;
@@ -81,42 +85,42 @@ public class Astrology extends Widget {
         return initialVal;
     }
 
-    private static void checkThreeTypes(AstroSymbol[] symbols) throws IllegalArgumentException{
+    private static void checkThreeTypes(AstroSymbol[] symbols) throws IllegalArgumentException {
         if (symbols.length != EXPECTED_SIZE)
             throw new IllegalArgumentException("Astrology input size should be " + EXPECTED_SIZE);
         containsOneOfEach(symbols);
     }
 
-    private static void containsOneOfEach(AstroSymbol[] symbols) throws IllegalArgumentException{
+    private static void containsOneOfEach(AstroSymbol[] symbols) throws IllegalArgumentException {
         containsOneCelestial(symbols);
         containsOneElement(symbols);
         containsOneZodiac(symbols);
     }
 
-    private static void containsOneCelestial(AstroSymbol[] symbols){
+    private static void containsOneCelestial(AstroSymbol[] symbols) {
         int count = 0;
         for (AstroSymbol symbol : symbols)
             if (isCelestial(symbol)) count++;
         if (count > 1) throw new IllegalArgumentException("Only one celestial body allowed");
     }
 
-    private static void containsOneElement(AstroSymbol[] symbols){
+    private static void containsOneElement(AstroSymbol[] symbols) {
         int count = 0;
         for (AstroSymbol symbol : symbols)
             if (isElemental(symbol)) count++;
         if (count > 1) throw new IllegalArgumentException("Only one element allowed");
     }
 
-    private static void containsOneZodiac(AstroSymbol[] symbols){
+    private static void containsOneZodiac(AstroSymbol[] symbols) {
         int count = 0;
         for (AstroSymbol symbol : symbols)
             if (isZodiac(symbol)) count++;
         if (count > 1) throw new IllegalArgumentException("Only one zodiac allowed");
     }
 
-    private static AstroSymbol[] sort(AstroSymbol[] symbols){
+    private static AstroSymbol[] sort(AstroSymbol[] symbols) {
         AstroSymbol[] output = new AstroSymbol[symbols.length];
-        for (AstroSymbol symbol : symbols){
+        for (AstroSymbol symbol : symbols) {
             if (isElemental(symbol)) output[ELEMENT_INDEX] = symbol;
             else if (isCelestial(symbol)) output[CELESTIAL_INDEX] = symbol;
             else if (isZodiac(symbol)) output[ZODIAC_INDEX] = symbol;
@@ -124,8 +128,8 @@ public class Astrology extends Widget {
         return output;
     }
 
-    private static boolean isZodiac(AstroSymbol symbol){
-        switch (symbol){
+    private static boolean isZodiac(AstroSymbol symbol) {
+        switch (symbol) {
             case ARIES:
             case TAURUS:
             case GEMINI:
@@ -143,8 +147,8 @@ public class Astrology extends Widget {
         return false;
     }
 
-    private static boolean isCelestial(AstroSymbol symbol){
-        switch (symbol){
+    private static boolean isCelestial(AstroSymbol symbol) {
+        switch (symbol) {
             case SUN:
             case MOON:
             case MERCURY:
@@ -160,8 +164,8 @@ public class Astrology extends Widget {
         return false;
     }
 
-    private static boolean isElemental(AstroSymbol symbol){
-        switch (symbol){
+    private static boolean isElemental(AstroSymbol symbol) {
+        switch (symbol) {
             case FIRE:
             case EARTH:
             case AIR:
--- src/main/java/bomb/modules/ab/astrology/AstrologyController.java
+++ src/main/java/bomb/modules/ab/astrology/AstrologyController.java
@@ -2,148 +2,105 @@ package bomb.modules.ab.astrology;
 
 import bomb.abstractions.Resettable;
 import bomb.tools.pattern.facade.FacadeFX;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.ToggleGroup;
 
 import java.util.Arrays;
 
 public class AstrologyController implements Resettable {
-    private final AstroSymbol[] set;
+    private enum AstrologyIndex {
+        ELEMENTAL_INDEX, CELESTIAL_INDEX, ZODIAC_INDEX
+    }
 
-    @FXML
-    private ImageView fire, water, earth, air,
-            mercury, venus, mars, jupiter, saturn, uranus, neptune, pluto, sun, moon,
-            aquarius, aries, cancer, capricorn, gemini, leo, libra, pisces, sag, scorpio, taurus, virgo;
+    private final AstroSymbol[] astroSymbolBuffer;
 
-    @FXML
-    private TextField omen;
+    @FXML private JFXButton resetButton;
 
-    @FXML
-    private Button astroReset;
+    @FXML private JFXTextField omenTextField;
+
+    @FXML private ToggleGroup elementGroup, celestialGroup, zodiacGroup;
 
     public AstrologyController() {
-        set = new AstroSymbol[3];
+        astroSymbolBuffer = new AstroSymbol[3];
     }
 
     @FXML
-    private void getElement(){
-        if (fire.isHover()){
-            set[0] = AstroSymbol.FIRE;
-        } else if (earth.isHover()){
-            set[0] = AstroSymbol.EARTH;
-        } else if (air.isHover()){
-            set[0] = AstroSymbol.AIR;
-        } else if (water.isHover()){
-            set[0] = AstroSymbol.WATER;
+    private void determineElement() {
+        enableResetButton();
+        setBufferLocation(elementGroup, AstroSymbol.getElementalSymbols(), AstrologyIndex.ELEMENTAL_INDEX);
+    }
+
+    @FXML
+    private void determineCelestial() {
+        enableResetButton();
+        setBufferLocation(celestialGroup, AstroSymbol.getCelestialSymbols(), AstrologyIndex.CELESTIAL_INDEX);
+    }
+
+    @FXML
+    private void determineZodiac() {
+        enableResetButton();
+        setBufferLocation(zodiacGroup, AstroSymbol.getZodiacSymbols(), AstrologyIndex.ZODIAC_INDEX);
+    }
+
+    private void enableResetButton() {
+        if (resetButton.isDisable())
+            FacadeFX.enable(resetButton);
+    }
+
+    private void setBufferLocation(ToggleGroup currentGroup, AstroSymbol[] searchArray, AstrologyIndex index) {
+        if (currentGroup.getSelectedToggle() == null) {
+            astroSymbolBuffer[index.ordinal()] = null;
+            disableResetButton();
+            return;
         }
 
-        elementDisable(true);
-        getOmen();
-    }
-
-    @FXML
-    private void getZodiac(){
-        if (aquarius.isHover()){
-            set[2] = AstroSymbol.AQUARIUS;
-        } else if (aries.isHover()){
-            set[2] = AstroSymbol.ARIES;
-        } else if (cancer.isHover()){
-            set[2] = AstroSymbol.CANCER;
-        } else if (capricorn.isHover()){
-            set[2] = AstroSymbol.CAPRICORN;
-        } else if (gemini.isHover()){
-            set[2] = AstroSymbol.GEMINI;
-        } else if (leo.isHover()){
-            set[2] = AstroSymbol.LEO;
-        } else if (libra.isHover()){
-            set[2] = AstroSymbol.LIBRA;
-        } else if (pisces.isHover()){
-            set[2] = AstroSymbol.PISCES;
-        } else if (sag.isHover()){
-            set[2] = AstroSymbol.SAGITTARIUS;
-        } else if (scorpio.isHover()){
-            set[2] = AstroSymbol.SCORPIO;
-        } else if (taurus.isHover()){
-            set[2] = AstroSymbol.TAURUS;
-        } else if (virgo.isHover()){
-            set[2] = AstroSymbol.VIRGO;
+        String toggleName = FacadeFX.getToggleName(currentGroup).toUpperCase();
+        for (int i = 0; i < searchArray.length; i++) {
+            if (toggleName.equals(searchArray[i].name())) {
+                astroSymbolBuffer[index.ordinal()] = searchArray[i];
+                i = searchArray.length;
+            }
         }
-        zodiacDisable(true);
-        getOmen();
+
+        if (astroSymbolBuffer[AstrologyIndex.ELEMENTAL_INDEX.ordinal()] != null &&
+                astroSymbolBuffer[AstrologyIndex.CELESTIAL_INDEX.ordinal()] != null &&
+                astroSymbolBuffer[AstrologyIndex.ZODIAC_INDEX.ordinal()] != null)
+            processBuffer();
     }
 
-    @FXML
-    private void getCeleste(){
-        if (mercury.isHover()){
-            set[1] = AstroSymbol.MERCURY;
-        } else if (venus.isHover()){
-            set[1] = AstroSymbol.VENUS;
-        } else if (mars.isHover()){
-            set[1] = AstroSymbol.MARS;
-        } else if (jupiter.isHover()){
-            set[1] = AstroSymbol.JUPITER;
-        } else if (saturn.isHover()){
-            set[1] = AstroSymbol.SATURN;
-        } else if (uranus.isHover()){
-            set[1] = AstroSymbol.URANUS;
-        } else if (neptune.isHover()){
-            set[1] = AstroSymbol.NEPTUNE;
-        } else if (pluto.isHover()){
-            set[1] = AstroSymbol.PLUTO;
-        } else if (sun.isHover()){
-            set[1] = AstroSymbol.SUN;
-        } else if (moon.isHover()){
-            set[1] = AstroSymbol.MOON;
-        }
-        celesteDisable(true);
-        getOmen();
+    private void disableResetButton() {
+        resetButton.setDisable(
+                astroSymbolBuffer[AstrologyIndex.ELEMENTAL_INDEX.ordinal()] == null &&
+                        astroSymbolBuffer[AstrologyIndex.CELESTIAL_INDEX.ordinal()] == null &&
+                        astroSymbolBuffer[AstrologyIndex.ZODIAC_INDEX.ordinal()] == null
+        );
     }
 
-    @FXML
-    private void astroReset(){
-        elementDisable(false);
-        celesteDisable(false);
-        zodiacDisable(false);
-        astroReset.setDisable(true);
-        Arrays.fill(set, null);
-    }
-
-    private void elementDisable(boolean set){
-        toggleImageViewArray(set, fire, earth, air, water);
-    }
-
-    private void celesteDisable(boolean set){
-        toggleImageViewArray(set, sun, moon, mercury, venus, mars, jupiter, saturn, uranus, neptune, pluto);
-    }
-
-    private void zodiacDisable(boolean set){
-        toggleImageViewArray(set, aquarius, aries, cancer, capricorn, gemini, leo, libra, pisces, sag, scorpio,
-                taurus, virgo);
-    }
-
-    private void toggleImageViewArray(boolean set, ImageView ... views){
-        if (set) FacadeFX.disableMultiple(views);
-        else FacadeFX.enableMultiple(views);
-    }
-
-    private void getOmen(){
+    private void processBuffer() {
         try {
-            if (set[0] != null && set[1] != null && set[2] != null) {
-                omen.setText(Astrology.calculate(set[0], set[1], set[2]));
-                astroReset();
-                astroReset.setDisable(true);
-            } else
-                astroReset.setDisable(false);
+            omenTextField.setText(Astrology.calculate(
+                    astroSymbolBuffer[0], astroSymbolBuffer[1], astroSymbolBuffer[2])
+            );
         } catch (IllegalArgumentException illegal){
-            FacadeFX.setAlert(Alert.AlertType.ERROR, "The Serial Code wasn't set");
+            FacadeFX.setAlert(Alert.AlertType.ERROR, illegal.getMessage());
         }
+    }
+
+    @FXML
+    private void resetModule() {
+        Arrays.fill(astroSymbolBuffer, null);
+        FacadeFX.disable(resetButton);
+        FacadeFX.clearText(omenTextField);
+        FacadeFX.resetToggleGroup(elementGroup);
+        FacadeFX.resetToggleGroup(celestialGroup);
+        FacadeFX.resetToggleGroup(zodiacGroup);
     }
 
     @Override
     public void reset() {
-        astroReset();
+        resetModule();
     }
 }
