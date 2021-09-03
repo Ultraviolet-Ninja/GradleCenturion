d603ed51b76ed4f760296c4fa8cf9f3442e80778
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-09-02 20:40:40 -0500

    Started on the fxml Bumped up the version preemptively

--- src/main/java/bomb/modules/s/switches/SwitchesController.java
+++ src/main/java/bomb/modules/s/switches/SwitchesController.java
@@ -1,8 +1,21 @@
 package bomb.modules.s.switches;
 
 import bomb.abstractions.Resettable;
+import com.jfoenix.controls.JFXRadioButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.controls.MFXToggleButton;
+import javafx.fxml.FXML;
 
 public class SwitchesController implements Resettable {
+    @FXML
+    private JFXRadioButton firstLight, secondLight, thirdLight, fourthLight, fifthLight;
+
+    @FXML
+    private MFXToggleButton firstSwitch, secondSwitch, thirdSwitch, fourthSwitch, fifthSwitch;
+
+    @FXML
+    private MFXTextField outputField;
+
     @Override
     public void reset() {
 
--- src/test/java/bomb/modules/s/switches/SwitchesTest.java
+++ src/test/java/bomb/modules/s/switches/SwitchesTest.java
@@ -24,7 +24,8 @@ public class SwitchesTest {
     @DataProvider
     public Object[][] trainingVideoProvider() {
         return new Object[][]{
-                {25, 2, new String[]{"1", "2", "5", "4"}}, {21, 9, new String[]{"1", "3", "2"}}
+                {25, 2, new String[]{"1", "2", "5", "4"}}, {21, 9, new String[]{"1", "3", "2"}},
+                {6, 31, new String[]{"3", "4", "5", "2", "1", "4", "3"}}
 //                {17, 22, new String[]{"5", "3", "4"}}
         };
     }
