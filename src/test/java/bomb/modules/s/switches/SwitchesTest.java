e99b856a3a4cd6170d7f2c4546aa9c6bb1e56b91
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-09-02 20:14:16 -0500

    Made tests

--- src/test/java/bomb/modules/s/switches/SwitchesTest.java
+++ src/test/java/bomb/modules/s/switches/SwitchesTest.java
@@ -3,6 +3,11 @@ package bomb.modules.s.switches;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
 public class SwitchesTest {
     @DataProvider
     public Object[][] exceptionTestDataProvider() {
@@ -19,12 +24,15 @@ public class SwitchesTest {
     @DataProvider
     public Object[][] trainingVideoProvider() {
         return new Object[][]{
-                {}, {}
+                {25, 2, new String[]{"1", "2", "5", "4"}}, {21, 9, new String[]{"1", "3", "2"}}
+//                {17, 22, new String[]{"5", "3", "4"}}
         };
     }
 
     @Test(dataProvider = "trainingVideoProvider")
-    public void trainingVideoTest() {
+    public void trainingVideoTest(int startingState, int desiredState, String[] expectedResult) {
+        List<String> converted = Arrays.asList(expectedResult);
 
+        assertEquals(converted, Switches.produceMoveList((byte) startingState, (byte) desiredState));
     }
 }
