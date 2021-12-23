3bbab54e5a718b87cb37a9d33245508168861c16
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-12-22 19:31:36 -0600

    Eliminated exception

--- src/main/java/bomb/TestingArea.java
+++ src/main/java/bomb/TestingArea.java
@@ -2,10 +2,8 @@ package bomb;
 
 import bomb.tools.filter.Regex;
 
-import java.io.IOException;
-
 public class TestingArea {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Regex labelFilter = new Regex("\"([^\"]*)\",?");
         Regex frequencyFilter = new Regex("frequencies\\.put\\(\"([^\"]+)\", (\\d\\.\\d{1,3})\\);");
         Regex whoMapFilter = new Regex("stepTwoMap\\.put\\(\"([^\"]+)\", \"([^\"]+)\"\\);");
