45a9be01d73d54eb71079a4819bea236c04958ee
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-12-23 09:39:50 -0600

    Optimized prime XD

--- src/main/java/bomb/tools/number/MathUtils.java
+++ src/main/java/bomb/tools/number/MathUtils.java
@@ -3,21 +3,17 @@ package bomb.tools.number;
 public class MathUtils {
     public static final int HASHING_NUMBER = 5501;
 
-    public static boolean isPerfectSquare(int num) {
+    public static boolean isPerfectSquare(long num) {
         double root = Math.sqrt(num);
         return (root - Math.floor(root)) == 0;
     }
 
-    public static boolean isPrime(int num) {
-        if (num <= 1)
-            return false;
-        if (num <= 3)
-            return true;
-        if(num % 2 == 0 || num % 3 == 0)
-            return false;
+    public static boolean isPrime(long n) {
+        if(n % 2 == 0 || n % 3 == 0)
+            return n <= 3 && n > 1;
 
-        for (int i = 5; i * i <= num; i += 6) {
-            if (num % i == 0 || num % (i + 2) == 0)
+        for (long i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
                 return false;
         }
         return true;
