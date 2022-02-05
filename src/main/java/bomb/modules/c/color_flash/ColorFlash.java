eabf3d458c2f84fb880720ae812f63a0efb25cb2
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-02-04 18:18:25 -0600

    Started a rework for ColorFlash

--- src/main/java/bomb/modules/c/color_flash/ColorFlash.java
+++ src/main/java/bomb/modules/c/color_flash/ColorFlash.java
@@ -1,102 +1,100 @@
 package bomb.modules.c.color_flash;
 
 import bomb.Widget;
+import org.javatuples.Pair;
+import org.jetbrains.annotations.NotNull;
 
-//TODO - Complete, then make Javadocs
+import java.util.ArrayList;
+import java.util.List;
+
+import static bomb.modules.c.color_flash.ColorFlashColor.BLUE;
+import static bomb.modules.c.color_flash.ColorFlashColor.GREEN;
+import static bomb.modules.c.color_flash.ColorFlashColor.RED;
+import static bomb.modules.c.color_flash.ColorFlashColor.WHITE;
+import static bomb.tools.string.StringFormat.NO;
+import static bomb.tools.string.StringFormat.YES;
 
 public class ColorFlash extends Widget {
-    static class Combo {
-        private final ColorFlashProperties.Word word;
-        private final ColorFlashProperties.Color color;
+    private static final int COMBO_LIMIT = 8;
 
-        Combo(ColorFlashProperties.Word word, ColorFlashProperties.Color color) {
-            this.color = color;
-            this.word = word;
-        }
+    private static final String RESULT_IS_COLOR_OR_WORD = "is either the word or color";
 
-        public ColorFlashProperties.Word getWord() {
-            return word;
-        }
+    public static String decipher(@NotNull ColorFlashColor[] words, @NotNull ColorFlashColor[] colors) {
+        validateInput(words, colors);
+        var comboList = forgeCombos(words, colors);
+        ColorFlashColor lastColor = comboList.get(comboList.size() - 1).getValue1();
 
-        public ColorFlashProperties.Color getColor() {
-            return color;
-        }
+        return switch (lastColor) {
+            case RED -> handleLastColorRed(comboList);
+            case YELLOW -> handleLastColorYellow(comboList);
+            case GREEN -> handleLastColorGreen(comboList);
+            case BLUE -> handleLastColorBlue(comboList);
+            case MAGENTA -> handleLastColorMagenta(comboList);
+            default -> handleLastColorWhite(comboList);
+        };
     }
 
-//    public static String decypher(CFlash.Word[] words, CFlash.Color[] colors) {
-//        Combo[] combos = forgeCombos(words, colors);
-//        String output;
-//
-//       switch (words[words.length - 1]) {
-//            case RED -> {
-//                if (iterateWord(combos, CFlash.Word.GREEN) >= 3)
-//                    return "Press Yes on the 3rd time Green is used as either the word or color";
-//                else if (iterateColor(combos, CFlash.Color.BLUE) == 1)
-//                   return "Press No when the word is Magenta";
-//                return "Press Yes on the last time White is a word or color";
-//            }
-//            case YELLOW -> {
-//                if (matches(combos, CFlash.Color.GREEN, CFlash.Word.BLUE))
-//                    return  "Press Yes on the first Green Color";
-//                else if (matches(combos, CFlash.Color.WHITE, CFlash.Word.WHITE) ||
-//                        matches(combos, CFlash.Color.RED, CFlash.Word.WHITE))
-//                    return  "";
-//                return "";
-//            }
-//            case GREEN -> {
-//                if ()
-//                    return "";
-//                else if (iterateWord(combos, CFlash.Word.MAGENTA) >= 3)
-//                    return "";
-//                return "";
-//            }
-//            case BLUE -> {
-//
-//            }
-//            case MAGENTA -> {
-//
-//            }
-//            default -> {
-//
-//            }
-//        }
-//        return output;
-//    }
+    private static String handleLastColorRed(List<Pair<ColorFlashColor, ColorFlashColor>> comboList) {
+        if (countParticularWord(comboList, GREEN) >= 3)
+            return YES + " on the third time Green " + RESULT_IS_COLOR_OR_WORD;
+        if (countParticularColor(comboList, BLUE) == 1)
+            return NO + " when the word Magenta is shown";
+        return YES + " the last time White " + RESULT_IS_COLOR_OR_WORD;
+    }
 
-    private static Combo[] forgeCombos(ColorFlashProperties.Word[] words, ColorFlashProperties.Color[] colors) {
-        Combo[] combos = new Combo[8];
-        for (int i = 0; i < 8; i++) {
-            combos[i] = new Combo(words[i], colors[i]);
+    private static String handleLastColorYellow(List<Pair<ColorFlashColor, ColorFlashColor>> comboList) {
+        if (doesMatchExists(comboList, GREEN, BLUE))
+            return "";
+        if (doesMatchExists(comboList, WHITE, WHITE) || doesMatchExists(comboList, RED, WHITE))
+            return "";
+        return "";
+    }
+
+    private static String handleLastColorGreen(List<Pair<ColorFlashColor, ColorFlashColor>> comboList) {
+        return "";
+    }
+
+    private static String handleLastColorBlue(List<Pair<ColorFlashColor, ColorFlashColor>> comboList) {
+        return "";
+    }
+
+    private static String handleLastColorMagenta(List<Pair<ColorFlashColor, ColorFlashColor>> comboList) {
+        return "";
+    }
+
+    private static String handleLastColorWhite(List<Pair<ColorFlashColor, ColorFlashColor>> comboList) {
+        return "";
+    }
+
+    private static List<Pair<ColorFlashColor, ColorFlashColor>> forgeCombos(ColorFlashColor[] words,
+                                                                            ColorFlashColor[] colors) {
+        List<Pair<ColorFlashColor, ColorFlashColor>> combos = new ArrayList<>(COMBO_LIMIT);
+        for (int i = 0; i < COMBO_LIMIT; i++) {
+            combos.add(new Pair<>(words[i], colors[i]));
         }
         return combos;
     }
 
-    private static int iterateWord(Combo[] combos, ColorFlashProperties.Word word) {
-        int found = 0;
-        for (Combo combo : combos) {
-            if (combo.getWord() == word) {
-                found++;
-            }
-        }
-        return found;
+    private static int countParticularWord(List<Pair<ColorFlashColor, ColorFlashColor>> combos, ColorFlashColor word) {
+        return (int) combos.stream()
+                .filter(pair -> pair.getValue0() == word)
+                .count();
     }
 
-    private static int iterateColor(Combo[] combos, ColorFlashProperties.Color color) {
-        int found = 0;
-        for (Combo combo : combos) {
-            if (combo.getColor() == color) {
-                found++;
-            }
-        }
-        return found;
+    private static int countParticularColor(List<Pair<ColorFlashColor, ColorFlashColor>> combos, ColorFlashColor color) {
+        return (int) combos.stream()
+                .filter(pair -> pair.getValue1() == color)
+                .count();
     }
 
-    private static boolean matches(Combo[] combos, ColorFlashProperties.Color color, ColorFlashProperties.Word word) {
-        for (Combo combo : combos) {
-            if (combo.getWord() == word && combo.getColor() == color) {
-                return true;
-            }
-        }
-        return false;
+    private static boolean doesMatchExists(List<Pair<ColorFlashColor, ColorFlashColor>> combos,
+                                           ColorFlashColor color, ColorFlashColor word) {
+        return combos.stream()
+                .anyMatch(pair -> pair.getValue0() == word && pair.getValue1() == color);
+    }
+
+    private static void validateInput(ColorFlashColor[] words, ColorFlashColor[] colors) {
+        if (words.length != colors.length && words.length != COMBO_LIMIT)
+            throw new IllegalArgumentException("Input is not of the specified length: 8");
     }
 }
--- src/main/java/bomb/tools/string/StringFormat.java
+++ src/main/java/bomb/tools/string/StringFormat.java
@@ -6,7 +6,8 @@ import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.joining;
 
 public class StringFormat {
-    public static final String BULLET_POINT = "\\u2022 ", ARROW = " -> ";
+    public static final String BULLET_POINT = "\\u2022 ", ARROW = " -> ",
+            YES = "Yes", NO = "No";
     public static final UnaryOperator<String> FIRST_LETTER_CAPITAL = sample -> sample.length() > 1 ?
             sample.substring(0, 1).toUpperCase() + sample.substring(1).toLowerCase() :
             sample.toUpperCase();
