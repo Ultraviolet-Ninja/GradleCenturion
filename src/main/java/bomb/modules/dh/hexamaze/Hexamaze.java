ec97c6aa8edd034f8c0b48431adb5b7a8d9da446
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-02-07 23:39:37 -0600

    Rewrote what parameters Hexamaze takes in and output to remove the front end component dependency

--- src/main/java/bomb/modules/dh/hexamaze/Hexamaze.java
+++ src/main/java/bomb/modules/dh/hexamaze/Hexamaze.java
@@ -1,7 +1,6 @@
 package bomb.modules.dh.hexamaze;
 
 import bomb.Widget;
-import bomb.components.hex.HexTile;
 import bomb.modules.dh.hexamaze.hexalgorithm.maze_finding.MazeSearch;
 import bomb.modules.dh.hexamaze.hexalgorithm.pathfinding.ExitChecker;
 import bomb.modules.dh.hexamaze.hexalgorithm.pathfinding.MazeRunner;
@@ -10,17 +9,16 @@ import bomb.modules.dh.hexamaze.hexalgorithm.storage.HexNode;
 import bomb.modules.dh.hexamaze.hexalgorithm.storage.HexagonalPlane;
 import bomb.modules.dh.hexamaze.hexalgorithm.storage.Maze;
 import bomb.tools.Coordinates;
-import bomb.tools.data.structures.queue.BufferedQueue;
 import javafx.scene.paint.Color;
 import org.javatuples.Pair;
+import org.javatuples.Quartet;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static bomb.components.hex.HexTile.DEFAULT_BACKGROUND_COLOR;
-import static java.util.stream.Collectors.toList;
 import static javafx.scene.paint.Color.BLUE;
 import static javafx.scene.paint.Color.CYAN;
 import static javafx.scene.paint.Color.GREEN;
@@ -42,11 +40,9 @@ public class Hexamaze extends Widget {
         COLOR_MAP.put(PINK, 5);
     }
 
-    public static String solve(@NotNull List<HexTile> tileList) throws IllegalArgumentException {
+    public static Quartet<@NotNull Grid,  @Nullable String, @Nullable Integer, @Nullable List<Coordinates>> solve(
+            @NotNull List<HexNode> nodeList) {
         Maze maze = new Maze();
-        List<HexNode> nodeList = tileList.stream()
-                .map(HexTile::getInternalNode)
-                .collect(toList());
         Grid original = new Grid(new HexagonalPlane(nodeList));
 
         Grid found = MazeSearch.search(maze, original);
@@ -54,14 +50,16 @@ public class Hexamaze extends Widget {
             throw new IllegalArgumentException("Could not find maze from given shapes");
 
         int colorValue = copyPegLocation(original, found);
-        setHexTileWalls(tileList, found);
         Pair<String, List<Coordinates>> exitInfo = ExitChecker.findPossibleExits(found);
         if (exitInfo == null)
-            return "";
+            return new Quartet<>(found, null, null, null);
 
-        List<Coordinates> resultingPath = MazeRunner.runMaze(found, exitInfo.getValue1());
-        fillHexTiles(tileList, resultingPath, colorValue);
-        return exitInfo.getValue0();
+        return new Quartet<>(
+                found,
+                exitInfo.getValue0(),
+                colorValue,
+                MazeRunner.runMaze(found, exitInfo.getValue1())
+        );
     }
 
     private static int copyPegLocation(Grid original, Grid found) {
@@ -77,33 +75,5 @@ public class Hexamaze extends Widget {
             }
         }
         return -1;
-    }
-
-    private static void setHexTileWalls(List<HexTile> tileList, Grid found) {
-        List<HexNode> foundNodes = found.getHexagon().asList();
-
-        int size = foundNodes.size();
-        for (int i = 0; i < size; i++) {
-            tileList.get(i).setInternalNode(foundNodes.get(i));
-        }
-    }
-
-    private static void fillHexTiles(List<HexTile> tileList, List<Coordinates> coordinatesList, int colorValue) {
-        Color color = RED;
-
-        for (Map.Entry<Color, Integer> entry : COLOR_MAP.entrySet()) {
-            if (entry.getValue() == colorValue)
-                color = entry.getKey();
-        }
-
-        for (HexTile hexTile : tileList)
-            hexTile.setBackgroundFill(DEFAULT_BACKGROUND_COLOR);
-
-        BufferedQueue<BufferedQueue<HexTile>> tileQueues = HexagonalPlane.convertFromList(tileList);
-
-        Color finalColor = color;
-        coordinatesList.stream()
-                .map(c -> tileQueues.get(c.x()).get(c.y()))
-                .forEach(tile -> tile.setBackgroundFill(finalColor));
     }
 }
--- src/main/java/bomb/modules/dh/hexamaze/HexamazeController.java
+++ src/main/java/bomb/modules/dh/hexamaze/HexamazeController.java
@@ -1,15 +1,29 @@
 package bomb.modules.dh.hexamaze;
 
 import bomb.abstractions.Resettable;
+import bomb.components.hex.HexTile;
 import bomb.components.hex.MazeComponent;
+import bomb.modules.dh.hexamaze.hexalgorithm.storage.Grid;
+import bomb.modules.dh.hexamaze.hexalgorithm.storage.HexNode;
+import bomb.modules.dh.hexamaze.hexalgorithm.storage.HexagonalPlane;
+import bomb.tools.Coordinates;
+import bomb.tools.data.structures.queue.BufferedQueue;
 import bomb.tools.pattern.facade.FacadeFX;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.paint.Color;
+import org.javatuples.Quartet;
 
+import java.util.List;
+import java.util.Map;
+
+import static bomb.components.hex.HexTile.DEFAULT_BACKGROUND_COLOR;
+import static bomb.modules.dh.hexamaze.Hexamaze.COLOR_MAP;
 import static bomb.tools.pattern.facade.FacadeFX.GET_TOGGLE_NAME;
+import static javafx.scene.paint.Color.RED;
 
 public class HexamazeController implements Resettable {
     @FXML
@@ -46,15 +60,55 @@ public class HexamazeController implements Resettable {
     @FXML
     private void solveMaze() {
         try {
-            String exitDirectionText = Hexamaze.solve(mazeComponent.createTileList());
-            exitDirectionLabel.setText(
-                    exitDirectionText.isEmpty() ?
-                            exitDirectionText :
-                            "Exit out of the " + exitDirectionText + " side"
-            );
+            List<HexTile> hexTileList = mazeComponent.createTileList();
+            List<HexNode> nodeList = hexTileList.stream()
+                    .map(HexTile::getInternalNode)
+                    .toList();
+
+            Quartet<Grid, String, Integer, List<Coordinates>> exitInfo = Hexamaze.solve(nodeList);
+            setHexTileWalls(hexTileList, exitInfo.getValue0());
+            String exitDirectionText = exitInfo.getValue1();
+            if (exitDirectionText != null) {
+                exitDirectionLabel.setText(
+                        exitDirectionText.isEmpty() ?
+                                exitDirectionText :
+                                "Exit out of the " + exitDirectionText + " side"
+                );
+
+                fillHexTiles(hexTileList, exitInfo.getValue3(), exitInfo.getValue2());
+            }
+
         } catch (IllegalArgumentException illegal) {
             FacadeFX.setAlert(Alert.AlertType.ERROR, illegal.getMessage());
         }
+    }
+
+    private static void setHexTileWalls(List<HexTile> tileList, Grid found) {
+        List<HexNode> foundNodes = found.getHexagon().asList();
+
+        int size = foundNodes.size();
+        for (int i = 0; i < size; i++) {
+            tileList.get(i).setInternalNode(foundNodes.get(i));
+        }
+    }
+
+    private static void fillHexTiles(List<HexTile> tileList, List<Coordinates> coordinatesList, int colorValue) {
+        Color color = RED;
+
+        for (Map.Entry<Color, Integer> entry : COLOR_MAP.entrySet()) {
+            if (entry.getValue() == colorValue)
+                color = entry.getKey();
+        }
+
+        for (HexTile hexTile : tileList)
+            hexTile.setBackgroundFill(DEFAULT_BACKGROUND_COLOR);
+
+        BufferedQueue<BufferedQueue<HexTile>> tileQueues = HexagonalPlane.convertFromList(tileList);
+
+        Color finalColor = color;
+        coordinatesList.stream()
+                .map(c -> tileQueues.get(c.x()).get(c.y()))
+                .forEach(tile -> tile.setBackgroundFill(finalColor));
     }
 
     @Override
