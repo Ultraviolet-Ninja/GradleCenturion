74c11f831c6596dd2bb07e3731532f70fa5997a1
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-01-16 10:08:29 -0600

    Update jlink-linux.sh

--- jlink-linux.sh
+++ jlink-linux.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 jlink \
 --module-path src/main/java \
---add-modules java.base,javafx.controls,javafx.fxml,\
-opencsv,commons-lang3 \
---output baselayer \
+--output base-layer \
+#--add-modules java.base,javafx.controls,javafx.fxml,\
+#opencsv,commons-lang3 \
+
 #--compress=2 \
 #--no-header-files \
 #--no-man-pages \
