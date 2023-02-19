c8a2f97da4d4100c5a58e8e0e1899a9047d83760
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-02-19 12:12:28 -0600

    Rewrote the test to make it easier to add more tests

--- src/test/java/bomb/modules/ab/battleship/BattleshipValidationTest.java
+++ src/test/java/bomb/modules/ab/battleship/BattleshipValidationTest.java
@@ -1,5 +1,6 @@
 package bomb.modules.ab.battleship;
 
+import bomb.ConditionSetter;
 import bomb.Widget;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
@@ -10,9 +11,13 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
+import static bomb.enumerations.Indicator.BOB;
+import static bomb.enumerations.Indicator.CAR;
 import static bomb.enumerations.Indicator.FRQ;
+import static bomb.enumerations.Indicator.SIG;
 import static bomb.enumerations.Port.PARALLEL;
 import static bomb.enumerations.Port.SERIAL;
+import static bomb.enumerations.TrinarySwitch.OFF;
 import static bomb.enumerations.TrinarySwitch.ON;
 import static bomb.modules.ab.battleship.Tile.CLEAR;
 import static bomb.modules.ab.battleship.Tile.RADAR;
@@ -22,22 +27,35 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class BattleshipValidationTest {
+    private static final ConditionSetter FIRST_BOMB_CONDITIONS =
+            BattleshipValidationTest::setVideoEdgeworkVersionOne;
+    private static final ConditionSetter SECOND_BOMB_CONDITIONS =
+            BattleshipValidationTest::setVideoEdgeworkVersionTwo;
     @BeforeMethod
     public void setUp() {
         Widget.resetProperties();
         Battleship.reset();
     }
 
-    @Test
-    public void videoTestCalculateValidRadarPositionsTest() {
-        setVideoEdgework();
-        Tile[][] expectedOcean = new Tile[][]{
-                {RADAR, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN},
-                {UNKNOWN, RADAR, UNKNOWN, UNKNOWN, UNKNOWN},
-                {UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN},
-                {UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN},
-                {UNKNOWN, RADAR, UNKNOWN, UNKNOWN, UNKNOWN}
+    @DataProvider
+    public Object[][] videoTestCalculateValidRadarPositionsTestProvider() {
+        return new Object[][] {
+                {
+                    FIRST_BOMB_CONDITIONS,
+                        new Tile[][]{
+                        {RADAR,   UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN},
+                        {UNKNOWN, RADAR,   UNKNOWN, UNKNOWN, UNKNOWN},
+                        {UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN},
+                        {UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN},
+                        {UNKNOWN, RADAR,   UNKNOWN, UNKNOWN, UNKNOWN}}
+                }
         };
+    }
+
+    @Test(dataProvider = "videoTestCalculateValidRadarPositionsTestProvider")
+    public void videoTestCalculateValidRadarPositionsTest(ConditionSetter setter,
+                                                          Tile[][] expectedOcean) {
+        setter.setCondition();
 
         Set<String> radarPositions = Battleship.calculateRadarPositions();
 
@@ -49,6 +67,7 @@ public class BattleshipValidationTest {
     public Object[][] videoOceanSolverTestProvider() {
         /*
          * Each test conforms to the following format
+         * The current bomb edgework
          * The Tiles of the radar locations
          * The row counters (The vertical values)
          * The column counter (Horizontal values)
@@ -57,6 +76,7 @@ public class BattleshipValidationTest {
          */
         return new Object[][]{
                 {
+                        FIRST_BOMB_CONDITIONS,
                         new Tile[]{CLEAR, CLEAR, SHIP}, new int[]{1, 2, 3, 0, 4},
                         new int[]{3, 1, 2, 1, 3}, new int[]{1, 1, 1, 1},
                         new Tile[][]{
@@ -68,6 +88,7 @@ public class BattleshipValidationTest {
                         }
                 },
                 {
+                        FIRST_BOMB_CONDITIONS,
                         new Tile[]{SHIP, CLEAR, SHIP}, new int[]{2, 1, 2, 1, 1},
                         new int[]{2, 2, 0, 3, 0}, new int[]{0, 1, 1, 2},
                         new Tile[][]{
@@ -79,6 +100,7 @@ public class BattleshipValidationTest {
                         }
                 },
                 {
+                        FIRST_BOMB_CONDITIONS,
                         new Tile[]{SHIP, CLEAR, SHIP}, new int[]{4, 0, 3, 1, 2},
                         new int[]{3, 3, 1, 1, 2}, new int[]{1, 0, 3, 0},
                         new Tile[][]{
@@ -90,6 +112,7 @@ public class BattleshipValidationTest {
                         }
                 },
                 {
+                        FIRST_BOMB_CONDITIONS,
                         new Tile[]{CLEAR, CLEAR, CLEAR}, new int[]{2, 1, 2, 1, 2},
                         new int[]{3, 0, 3, 2, 0}, new int[]{0, 1, 2, 1},
                         new Tile[][]{
@@ -104,9 +127,9 @@ public class BattleshipValidationTest {
     }
 
     @Test(dataProvider = "videoOceanSolverTestProvider")
-    public void videoOceanSolverTest(Tile[] confirmedRadarSpots, int[] rowCounters,
+    public void videoOceanSolverTest(ConditionSetter setter, Tile[] confirmedRadarSpots, int[] rowCounters,
                                      int[] columnCounters, int[] shipQuantities, Tile[][] expected) {
-        setVideoEdgework();
+        setter.setCondition();
         setShipQuantities(shipQuantities);
         Battleship.setRowCounters(rowCounters);
         Battleship.setColumnCounters(columnCounters);
@@ -116,6 +139,7 @@ public class BattleshipValidationTest {
 
         Set<Ocean> possibleOceans = Battleship.solveOcean();
 
+        assertEquals(possibleOceans.size(), 1);
         assertTrue(possibleOceans.contains(new Ocean(expected)));
     }
 
@@ -125,7 +149,7 @@ public class BattleshipValidationTest {
         Battleship.reset();
     }
 
-    private static void setVideoEdgework() {
+    private static void setVideoEdgeworkVersionOne() {
         Widget.setSerialCode("ZB6HA2");
         Widget.setDBatteries(2);
         Widget.setDoubleAs(2);
@@ -134,6 +158,16 @@ public class BattleshipValidationTest {
         Widget.setNumberOfPlates(1);
         Widget.setPortValue(PARALLEL, 1);
         Widget.setPortValue(SERIAL, 1);
+    }
+
+    private static void setVideoEdgeworkVersionTwo() {
+        Widget.setSerialCode("9X3VS6");
+        Widget.setIndicator(OFF, BOB);
+        Widget.setIndicator(OFF, FRQ);
+        Widget.setIndicator(OFF, CAR);
+        Widget.setIndicator(ON, SIG);
+        Widget.setDoubleAs(2);
+        Widget.setNumHolders(1);
     }
 
     private static void setShipQuantities(int[] shipQuantities) {
