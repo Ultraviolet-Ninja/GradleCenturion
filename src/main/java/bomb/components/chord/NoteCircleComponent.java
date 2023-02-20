6a328211c66f2caf583d3be2b1b2ec3ac36d7544
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-02-20 11:15:42 -0600

    Renamed variables

--- src/main/java/bomb/components/chord/NoteCircleComponent.java
+++ src/main/java/bomb/components/chord/NoteCircleComponent.java
@@ -31,7 +31,9 @@ public final class NoteCircleComponent extends Pane implements Resettable {
             noteDSharpLight, noteELight, noteFLight, noteFSharpLight, noteGLight, noteGSharpLight;
 
     @FXML
-    private MFXButton a, aSharp, b, c, cSharp, d, dSharp, e, f, fSharp, g, gSharp;
+    private MFXButton noteAButton, noteASharpButton, noteBButton, noteCButton, noteCSharpButton,
+            noteDButton, noteDSharpButton, noteEButton, noteFButton, noteFSharpButton,
+            noteGButton, noteGSharpButton;
 
     public NoteCircleComponent() {
         super();
@@ -45,7 +47,10 @@ public final class NoteCircleComponent extends Pane implements Resettable {
     }
 
     public void initialize() {
-        MFXButton[] buttonArray = {a, aSharp, b, c, cSharp, d, dSharp, e, f, fSharp, g, gSharp};
+        MFXButton[] buttonArray = {noteAButton, noteASharpButton, noteBButton, noteCButton,
+                noteCSharpButton, noteDButton, noteDSharpButton, noteEButton, noteFButton,
+                noteFSharpButton, noteGButton, noteGSharpButton};
+
         Circle[] circleArray = {
                 noteALight, noteASharpLight, noteBLight, noteCLight, noteCSharpLight, noteDLight,
                 noteDSharpLight, noteELight, noteFLight, noteFSharpLight, noteGLight, noteGSharpLight
