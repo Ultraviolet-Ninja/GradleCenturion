655c1bcf5eff7b4b44d9078a33af456bfd70d781
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-11-02 09:44:27 -0500

    Update StreamBootDrive.java

--- src/main/java/bomb/tools/boot/StreamBootDrive.java
+++ src/main/java/bomb/tools/boot/StreamBootDrive.java
@@ -14,16 +14,18 @@ import static java.util.stream.Collectors.toMap;
 
 public sealed abstract class StreamBootDrive implements FxmlBootDrive
         permits ParallelStreamBootDrive, SequentialStreamBootDrive {
-//    @SuppressWarnings("DataFlowIssue")
+    /*
+    @SuppressWarnings("DataFlowIssue")
+    */
     @Contract("_ -> new")
     @Override
     public @NotNull final SequencedMap<String, Region> createFXMLMap(ResetObserver resetObserver) {
         var displayClassStream = supplyStream();
-
-//        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-//            displayClassStream = displayClassStream.sequential();
-//        }
-
+    /*
+     *  if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+     *      displayClassStream = displayClassStream.sequential();
+     *  }
+     */
         return new LinkedHashMap<>(displayClassStream
                 .map(cls -> FxmlBootDrive.mapClassToRegion(cls, resetObserver))
                 .collect(toMap(Pair::getValue0, Pair::getValue1)));
