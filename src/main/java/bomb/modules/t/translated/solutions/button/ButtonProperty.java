b355fca6e50822b802616bd2bf7457b705f449af
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-11-04 14:11:03 -0500

    Added array groups per colors and per labels

--- src/main/java/bomb/modules/t/translated/solutions/button/ButtonProperty.java
+++ src/main/java/bomb/modules/t/translated/solutions/button/ButtonProperty.java
@@ -1,5 +1,18 @@
 package bomb.modules.t.translated.solutions.button;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 public enum ButtonProperty {
-    BLUE, RED, YELLOW, WHITE, ABORT, DETONATE, PRESS, HOLD
+    BLUE, RED, YELLOW, WHITE, ABORT, DETONATE, PRESS, HOLD;
+
+    @Contract(value = " -> new", pure = true)
+    public static ButtonProperty @NotNull [] getColors() {
+        return new ButtonProperty[]{RED, YELLOW, BLUE, WHITE};
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static ButtonProperty @NotNull [] getLabels() {
+        return new ButtonProperty[]{DETONATE, ABORT, PRESS, HOLD};
+    }
 }
