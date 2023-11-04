2abcc1e2c548e2054e71986847031775e6509cd3
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-11-04 14:14:05 -0500

    Made the interface sealed

--- src/main/java/bomb/modules/t/translated/solutions/TranslationComponent.java
+++ src/main/java/bomb/modules/t/translated/solutions/TranslationComponent.java
@@ -1,7 +1,11 @@
 package bomb.modules.t.translated.solutions;
 
+import bomb.modules.t.translated.solutions.button.ButtonComponent;
+import bomb.modules.t.translated.solutions.gas.VentGasComponent;
+import bomb.modules.t.translated.solutions.password.PasswordComponent;
+
 import java.util.List;
 
-public interface TranslationComponent {
+public sealed interface TranslationComponent permits ButtonComponent, PasswordComponent, VentGasComponent {
     void setContent(List<String> languageContent);
 }
