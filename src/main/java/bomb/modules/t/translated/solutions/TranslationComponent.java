a3193c9e303ec0bcbba7fe86f7ce4da8612f9bce
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2023-11-04 15:29:37 -0500

    Set up the DisplayComponent for the TranslationComponent

--- src/main/java/bomb/modules/t/translated/solutions/TranslationComponent.java
+++ src/main/java/bomb/modules/t/translated/solutions/TranslationComponent.java
@@ -1,11 +1,13 @@
 package bomb.modules.t.translated.solutions;
 
+import bomb.annotation.DisplayComponent;
 import bomb.modules.t.translated.solutions.button.ButtonComponent;
 import bomb.modules.t.translated.solutions.gas.VentGasComponent;
 import bomb.modules.t.translated.solutions.password.PasswordComponent;
 
 import java.util.List;
 
+@DisplayComponent(resource = "translated_vanilla_modules.fxml", buttonLinkerName = "Translated Vanilla Modules")
 public sealed interface TranslationComponent permits ButtonComponent, PasswordComponent, VentGasComponent {
     void setContent(List<String> languageContent);
 }
